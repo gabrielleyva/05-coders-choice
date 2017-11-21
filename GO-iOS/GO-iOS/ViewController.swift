caabfdd27f938fad297e413f5ea0fe0234b122bb
Gabo <gleyvamerino@smu.edu>
2017-11-20 19:52:16 -0600

    Xcode project added. Finally

---
+++ GO-iOS/GO-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GO-iOS
+//
+//  Created by Gabriel I Leyva Merino on 11/20/17.
+//  Copyright © 2017 Gabriel Leyva Merino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
